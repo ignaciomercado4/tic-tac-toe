a606fc19f91a6d4b8f73ab8291b3cdafeb1c4834
Ignacio Mercado <igmercado46@gmail.com>
2024-12-02 17:16:05 -0300

    adds initial print and user movement functions

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func printBoard(board [][]string) {
+	for i := 0; i < len(board); i++ {
+		fmt.Println(board[i][0], "|", board[i][1], "|", board[i][2])
+	}
+}
+
+func getUserMovement(board [][]string) {
+	fmt.Println("Current board state:")
+	printBoard(board)
+
+	var selectedTile int
+
+	fmt.Scan(&selectedTile)
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			if board[i][j] == fmt.Sprintf("%d", selectedTile) &&
+				board[i][j] != "X" &&
+				board[i][j] != "O" {
+
+				board[i][j] = "X"
+				return
+			}
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Init")
+	board := [][]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}}
+
+	getUserMovement(board)
+	printBoard(board)
 }
